e403b66164c4be59fc2574708651667d96c301e8
henrythomaz <henrythcampos@gmail.com>
2026-04-19 12:53:31 -0400

    feat: add main App component

---
+++ src/App.tsx
@@ -0,0 +1,114 @@
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+// páginas
+import Home from "./pages/Home.tsx";
+import Login from "./pages/Login.tsx";
+import Cadastro from "./pages/Cadastro.tsx";
+import Conta from "./pages/Conta.tsx";
+import ConfirmEmail from "./pages/ConfirmEmail.tsx";
+import Dashboard from "./pages/Dashboard.tsx";
+import Estacoes from "./pages/Estacoes.tsx";
+import UsuarioDetalhe from "./pages/UsuarioDetalhe";
+import Usuarios from "./pages/Usuarios.tsx";
+import DocsPage from "./pages/DocsPage.tsx";
+import DashboardIndex from "./pages/DashboardIndex";
+import Sobre from "./pages/Sobre";
+
+// layout
+import Layout from "./components/Layout.tsx";
+
+const PrivateRoute = ({ children }: any) => {
+  const isAuth = !!localStorage.getItem("token");
+  return isAuth ? children : <Navigate to="/login" />;
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+
+        {/* PUBLICO */}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/confirm-email" element={<ConfirmEmail />} />
+        <Route path="/docs" element={<DocsPage />} />
+        <Route path="/sobre" element={<Sobre />} />
+
+        {/* PRIVADO */}
+
+        <Route
+  path="/dashboard"
+  element={
+    <PrivateRoute>
+      <Layout>
+        <DashboardIndex />
+      </Layout>
+    </PrivateRoute>
+  }
+/>
+
+        <Route
+          path="/dashboard/:id"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <Dashboard />
+              </Layout>
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/estacoes"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <Estacoes />
+              </Layout>
+            </PrivateRoute>
+          }
+        />
+
+        {/* LISTA DE USUÁRIOS */}
+        <Route
+          path="/usuarios"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <Usuarios />
+              </Layout>
+            </PrivateRoute>
+          }
+        />
+
+        {/* DETALHE DO USUÁRIO */}
+        <Route
+          path="/usuarios/:id"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <UsuarioDetalhe />
+              </Layout>
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/conta"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <Conta />
+              </Layout>
+            </PrivateRoute>
+          }
+        />
+
+        {/* fallback */}
+        <Route path="*" element={<Navigate to="/" />} />
+
+      </Routes>
+    </Router>
+  );
+}
